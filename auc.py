e1abaa911527c5444e8865a5feb922dbcff57ee9
Emma Kong <yankon@microsoft.com>
2018-02-02 14:03:49 -0800

    add auc.py

---
+++ auc.py
@@ -0,0 +1,23 @@
+# simple python script to calculate AUC for binary classifcation
+# input: true labels, predicted scores
+# label: 1 - positivel; 0 - negative
+
+import numpy as np
+
+def auc(labels, scores, posLabel = 1):
+
+	sortIndex = np.argsort(scores)
+	# rank the labels by descending order of scores
+	mylabels = np.array(labels)[sortIndex[::-1]]
+	nPos = sum(mylabels == posLabel)
+	nNeg = len(mylabels) - nPos
+	pos = 0
+	neg = 0
+	area = 0
+	for i in range(len(mylabels)):
+		if mylabels[i] == 0:
+			neg += 1
+			area += pos
+		else:
+			pos += 1
+	return area  * 1.0 / (nPos * nNeg)
